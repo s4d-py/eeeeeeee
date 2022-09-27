ce1353b77dc65326a199e7c4a4ddaa1eb443cb54
frostzzone <frostzzone@gmail.com>
2022-09-27 09:33:22 -0700

    base 469 clone

---
+++ src/blocks/text/index.js
@@ -0,0 +1,8 @@
+import "./newline";
+import "./starts-with";
+import "./includes";
+import "./print";
+import "./ends-with";
+import "./weird-to-normal";
+import "./for_each_letter"
+import "./create_random_string"
